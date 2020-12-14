92d7605df4eb57ef19944610ec748a7d048eae95
silvianiiinimaki <silvia.niininmaki@hotmail.fi>
2020-12-14 11:40:18 +0200

    Add /api.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Lista from "./lista";
 import Lista1 from "./lista1";
 import ListPage from "./listPage";
 import CreateTask from "./createTask";
+import Api from "./api";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 
@@ -16,6 +17,9 @@ function App() {
           <CreateTask />
         </Route>
         <Route path="/editTask"></Route>
+        <Route path="/api">
+          <Api />
+        </Route>
       </Switch>
     </Router>
   );
