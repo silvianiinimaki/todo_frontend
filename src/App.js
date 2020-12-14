7be4ff6313e6466fa1abc66248f304cb5f483490
silvianiiinimaki <silvia.niininmaki@hotmail.fi>
2020-12-14 11:45:23 +0200

    Undo /api.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Lista from "./lista";
 import Lista1 from "./lista1";
 import ListPage from "./listPage";
 import CreateTask from "./createTask";
-import Api from "./api";
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 
@@ -17,9 +17,6 @@ function App() {
           <CreateTask />
         </Route>
         <Route path="/editTask"></Route>
-        <Route path="/api">
-          <Api />
-        </Route>
       </Switch>
     </Router>
   );
